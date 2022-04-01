9506e8bbb85ff62e5a140100e49d608ca3ee6a14
John <wjh5597486@gmail.com>
2022-04-01 17:23:37 +0900

    'sovle 1'

--- 13460.구슬탈출2___.py
+++ 13460.구슬탈출2___.py
@@ -10,15 +10,36 @@ b : blue
 the chances for moving are limited 10 times, then print(-1).
 """
 
+from collections import deque
+
 N, M = map(int, input().split())
-graph = [[] for i in range(M)]
+graph = [[] for i in range(N)]
 for i in range(N):
     text = input()
-    for j in range(len(text)):
+    for j in range(M):
         graph[i].append(text[j])
 
 for i in graph:
     print(i)
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == "R":
+            red = [i, j]
+        if graph[i][j] == "B":
+            blue = [i, j]
+        if graph[i][j] == "O":
+            hole = [i, j]
+
+dq = deque()
+dq.append([red, blue, hole])
+
+while dq:
+    red1, blu1, hol1 = dq.popleft()
+
+
+
+# 4 ways.
 
 # if the positions of two balls are the same, remove the case.
 # directions are not the same as previous direction. ( overlap )
--- Algorithm/4)BFS_DFS/2206.벽부수고rffsfdsfsfasdfasdfasdfy.py
+++ Algorithm/4)BFS_DFS/2206.벽부수고rffsfdsfsfasdfasdfasdfy.py
@@ -1,33 +1,52 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    row = list(str(input().rstrip()))
+    graph.append(list(map(int, row)))
 
-N, M = map(int, input().split())
-inf = float("inf")
-
-def go(G):
-    cost = [[inf] * M for _ in range(N)]
-    dq = []
-    dq.append([0, 0, 1, 0])
-    while dq:
-        n, m, c, d = dq.pop() # c: chance
-        for ni, mi in [[n + 1, m], [n, m + 1], [n - 1, m], [n, m - 1]]:
-            if not (0 <= ni < N and 0 <= mi < M):  # range
-                continue
-            if cost[ni][mi] < d+1:  # low_cost
-                continue
-            if G[ni][mi] == 1:  # chance
-                if c == 0:
-                    continue
-                else:
-                    cost[ni][mi] = d + 1
-                    dq.append([ni, mi, 0, d+1])
-            else:
-                cost[ni][mi] = d + 1
-                dq.append([ni, mi, c, d+1])
-
-    print(cost[-1][-1])
-
-go(graph)
+# 3차원 배열 사용 -> visited[x][y][z], z = 0 or 1로 기록
+# z = 0이면 벽을 뚫지 않고 간 경우, z = 1이면 벽을 뚫고 간 경우
+moves = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+]
+visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
 
 
+def bfs():
+    q = deque()
+    q.append([0, 0, 0])
+    visited[0][0][0] = 1
 
+    while q:
+        x, y, w = q.popleft()
+
+        # 목표지점 도달 시 해당 위치까지의 거리 리턴
+        if x == n - 1 and y == m - 1:
+            return visited[x][y][w]
+
+        for move in moves:
+            nx, ny = x + move[0], y + move[1]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                # 현재 위치로 이동할 수 있고, 아직 방문하지 않았다면
+                if graph[nx][ny] == 0 and visited[nx][ny][w] == 0:
+                    visited[nx][ny][w] = visited[x][y][w] + 1
+                    q.append([nx, ny, w])
+
+                # 현재 위치가 벽이고, 벽을 아직 부수지 않았다면
+                elif graph[nx][ny] == 1 and w == 0:
+                    visited[nx][ny][w + 1] = visited[x][y][w] + 1
+                    q.append([nx, ny, w + 1])
+
+    # 목표지점까지 도달하지 못한다면 -1 리턴
+    return -1
+
+
+print(bfs())
