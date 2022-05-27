276ab4f1fe15c4caeb864905a71c1022183358c5
John <97501876+wjh5597486@users.noreply.github.com>
2022-05-27 14:54:35 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 436 ms, Memory: 240416 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -0,0 +1,17 @@
+def solution():
+    A = input()
+    B = input()
+    r, c = len(A), len(B)
+    G = [[0]*(c+1) for _ in range(r+1)]
+    result = 0
+    for i in range(1,r+1):
+        for j in range(1,c+1):
+            if A[i-1] == B[j-1]:
+                G[i][j] = G[i-1][j-1] + 1
+                if G[i][j] > result : result = G[i][j]
+    print(result)
+
+if __name__ == "__main__":
+    solution()
+
+
