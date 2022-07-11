813783936a682197bb77fd8953f70ba0adc13ffe
Won ji hwan <97501876+wjh5597486@users.noreply.github.com>
2022-07-11 19:01:53 +0900

    [Gold V] Title: 토달기, Time: 1960 ms, Memory: 134136 KB -BaekjoonHub

---
+++ 백준/Gold/1897. 토달기/토달기.py
@@ -0,0 +1,42 @@
+import sys
+def solution():
+    N, text = input().split()
+    set_list = [[] for i in range(81)]
+    for i in range(int(N)):
+        word = sys.stdin.readline().strip()
+        set_list[len(word)].append(word)
+
+    def check(a, b):
+        j = 0
+        for i in range(len(a)):
+            if a[i] == b[i+j]:
+                continue
+            if j == 0:
+                j = 1
+            if a[i] == b[i+j]:
+                continue
+            return False
+        return True
+
+
+    text = [text]
+    i = 0
+    while i < len(text):
+        cur_word = text[i]
+        next_words = set_list[len(cur_word)+1]
+
+        for next_word in next_words:
+            if check(cur_word, next_word) is True:
+                text.append(next_word)
+
+        i += 1
+    print(text[-1])
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    solution()
